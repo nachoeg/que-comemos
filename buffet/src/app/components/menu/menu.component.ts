329a30d6958fffd3514dc96951c6ecd54c84340e
Marina Lencinas <marinaalp@gmail.com>
2024-12-16 22:43:34 -0300

    ver detalle del menu

--- buffet/src/app/components/menu/menu.component.ts
+++ buffet/src/app/components/menu/menu.component.ts
@@ -2,24 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { MenuesServicio } from '../../services/menues-servicio/menues-servicio';
 import { Menu } from '../../models/menu/menu';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
-  providers: [ MenuesServicio ]
+  providers: [MenuesServicio],
+
 })
-export class MenuComponent implements OnInit{
+export class MenuComponent implements OnInit {
+  isDetailVisible: boolean[] = [];
+
+
+
+
+  images = [
+    "/buffet-1.jpg",
+    "/buffet-2.jpg",
+    "/buffet-3.jpg",
+    "/buffet-4.jpg",
+    "/buffet-5.jpg"
+  ]
+
+  getRandomImageIndex() {
+    return Math.floor(Math.random() * this.images.length);
+  }
+
 
   menus: Menu[] = [];
   constructor(private menuService: MenuesServicio) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.menuService.getMenus().subscribe(menus => {
       this.menus = menus;
+      this.isDetailVisible = new Array(menus.length).fill(false);
     });
+
   }
 
 }
