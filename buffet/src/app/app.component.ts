46d09c3355b7faf12ebbd1113268ae7d56d479dd
nachoeg <nachoeg99@gmail.com>
2024-12-30 17:24:29 -0300

    Actualizo navbar

--- buffet/src/app/app.component.ts
+++ buffet/src/app/app.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginServicio } from './services/login-servicio/login-servicio';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   providers: [LoginServicio],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'buffet';
-
-  displaySideBar = true;
-
-  onToggleEvent(){
-    this.displaySideBar = !this.displaySideBar;
-  }
-
+  title = '¿Qué comemos?';
 }
--- buffet/src/app/components/navbar/navbar.component.ts
+++ buffet/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router, RouterModule } from '@angular/router';
 import { LoginServicio } from '../../services/login-servicio/login-servicio';
 import { CommonModule } from '@angular/common';
@@ -9,37 +9,25 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule, CommonModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    CommonModule,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
-
-  @Output() emiteToggleSidenav = new EventEmitter<void>();
-
-  emitToggleEvent(){
-    this.emiteToggleSidenav.emit();
-  }
-
   constructor(private router: Router, public loginServicio: LoginServicio) {}
-
-  navigateToLogin() {
-    this.router.navigate(['/login']);
-  }
-
-  navigateToRegister() {
-    this.router.navigate(['/register']);
-  }
 
   logout() {
     this.router.navigate(['/']);
-    this.loginServicio.logout(); 
+    this.loginServicio.logout();
   }
 
   get isLoggedIn(): boolean {
-    return this.loginServicio.isLogged(); 
-    
+    return this.loginServicio.isLogged();
   }
-
 }
